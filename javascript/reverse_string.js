8107b915f976c563a8d3db364d869a76df3e9b11
Alan Czemerinski <alanczeme@gmail.com>
2022-04-05 11:49:18 -0400

    finished solution

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,25 @@
+// write a method that receives one argument (string) and outputs that string with its letters in reverse order
 function reverseString(str) {
-  // type your code here
+  // if str = "", then return str
+  if(str === "") return str;
+
+  // create an empty array that will hold the reversed string (as an array)
+  let reversedString = [];
+
+  // convert str string argument to array
+  str = [...str];
+
+  // loop over the letters in the argument (str), starting from the beginning of the string
+  for(i in str) {
+    // add ("unshift") each letter to the BEGINNING of the reversed string variable
+    reversedString.unshift(str[i]);
+  }
+
+  // convert the reversed string array to a string
+  reversedString = reversedString.join('');
+
+  // return reversed string variable
+  return reversedString;
 }
 
 if (require.main === module) {
@@ -11,6 +31,16 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+
+  console.log("");
+
+  console.log("Expecting: 'emop'");
+  console.log("=>", reverseString("pome"));
+  
+  console.log("");
+
+  console.log("Expecting: ''");
+  console.log("=>", reverseString(""));
 }
 
 module.exports = reverseString;
